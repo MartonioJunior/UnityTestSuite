8942633e2edbc02db2cc6699ca9dfa8b447d7d2c
Martônio Júnior <martoniojunior@hotmail.com>
2023-05-01 20:51:06 -0300

    feat: Mock class

---
+++ Runtime/Mock/Mock.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+using NSubstitute;
+using System.Collections;
+using System;
+using Object = UnityEngine.Object;
+
+namespace MartonioJunior.Test
+{
+    public static partial class Mock
+    {
+        #region Variables
+        private static readonly List<Object> objectList = new();
+        #endregion
+        #region Properties
+        #endregion
+        #region Static Methods
+        public static void Clear()
+        {
+            objectList.ForEach(Object.DestroyImmediate);
+            objectList.Clear();
+        }
+
+        public static void Register(Object obj)
+        {
+            if (obj == null) return;
+
+            objectList.Add(obj);
+        }
+        #endregion
+    }
+}
